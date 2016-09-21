5dbe5ebe05d3643b7bb9238dd6f36f650ba69809
Pascal Pflaum <mail@pascalpflaum.de>
2016-09-21 22:48:11 +0200

    display the user on the map including circle displaying accuracy

--- src/view/presentational/mainMap.jsx
+++ src/view/presentational/mainMap.jsx
@@ -7,16 +7,24 @@ export default class MainMap extends React.Component {
 
     componentDidMount() {
         console.log('didMount', this.props);
-        var map = L.map('mainMap');
-        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar'}).addTo(map);
-        map.locate({setView: true, maxZoom: 16});
-        console.time('marker');
-         this.props.locations.forEach((location)=> {
+        this.leafLetMap = L.map('mainMap');
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(this.leafLetMap);
 
-             //Leaflet is using (north, east) or (latLng), but the backend stores it in the more common (east, north) or (lngLat) format
-             L.marker(location.location.coordinates.reverse()).addTo(map);
-         });
-        console.timeEnd('marker');
+        //geolocation
+        this.leafLetMap.locate({watch: true, setView: true, maxZoom: 16});
+        this.leafLetMap.on('locationfound', onLocationFound.bind(this.leafLetMap));
+
+        //marker and popup
+        this.props.locations.forEach((location)=> {
+
+            const title = location.usageTerm + ' (' + location.constructionYear + ')';
+            //Leaflet is using (north, east) or (latLng), but the backend stores it in the more common (east, north) or (lngLat) format
+            const marker = L.marker(location.location.coordinates.reverse(), {title, alt: title});
+            const popup = L.popup({closeButton: false}).setContent(title);
+
+            marker.bindPopup(popup);
+            marker.addTo(this.leafLetMap);
+        });
 
     }
 
@@ -24,4 +32,15 @@ export default class MainMap extends React.Component {
         console.log('render');
         return <div id="mainMap"></div>;
     }
+}
+
+/**
+ * display a marker and circle for the users position
+ * @param e
+ */
+function onLocationFound(e) {
+    console.log('onLocation');
+    var radius = e.accuracy / 2;
+    L.marker(e.latlng).addTo(this);
+    L.circle(e.latlng, radius).addTo(this);
 }
