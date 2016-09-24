3b24f9cc9351a6ba00e5b148ed8107c1f8a420c5
Pascal Pflaum <mail@pascalpflaum.de>
2016-09-24 14:57:04 +0200

    postinstall hooks

--- Gruntfile.js
+++ Gruntfile.js
@@ -60,11 +60,45 @@ module.exports = function (grunt) {
                 },
                 // stats: false disables the stats output
 
-                //storeStatsTo: "xyz", // writes the status to a variable named xyz
-                // you may use it later in grunt i.e. <%= xyz.hash %>
+                progress: false, // Don't show progress
 
-                progress: true, // Don't show progress
-                // Defaults to true
+                failOnError: true, // don't report error to grunt if webpack find errors
+                // Use this if webpack errors are tolerable and grunt should continue
+
+                watch: false, // use webpacks watcher
+                // You need to keep the grunt process alive
+
+                keepalive: false // don't finish the grunt task
+                // Use this in combination with the watch option
+
+            },
+
+            dev: {
+                // webpack options
+                entry: './src/view/main.jsx',
+                output: {
+                    path: './static/',
+                    filename: 'all.js'
+                },
+                module: {
+                    loaders: [
+                        {
+                            test: /.jsx?$/,
+                            loader: 'babel-loader',
+                            exclude: /node_modules/,
+                            query: {
+                                presets: ['es2015', 'react']
+                            }
+                        }
+                    ]
+                },
+
+                stats: {
+                    // Configure the console output
+                    colors: false,
+                    modules: false,
+                    reasons: true
+                },
 
                 failOnError: false, // don't report error to grunt if webpack find errors
                 // Use this if webpack errors are tolerable and grunt should continue
@@ -75,18 +109,12 @@ module.exports = function (grunt) {
                 keepalive: true // don't finish the grunt task
                 // Use this in combination with the watch option
 
-                //inline: true,  // embed the webpack-dev-server runtime into the bundle
-                // Defaults to false
-
-                //hot: true, // adds the HotModuleReplacementPlugin and switch the server to hot mode
-                // Use this in combination with the inline option
-
             }
         }
     });
 
 
-    grunt.registerTask('postinstall', ['webpack']);
+    grunt.registerTask('postinstall', ['webpack:build']);
     grunt.registerTask('unit', ['mochaTest:build']);
     grunt.registerTask('monitor', ['nodemon:build']);
     grunt.registerTask('hint', ['eslint']);
