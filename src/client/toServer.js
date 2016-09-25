d5d2ed95ecc28d8205004e5642f5756c78cc3b7f
Pascal Pflaum <mail@pascalpflaum.de>
2016-09-25 17:26:27 +0200

    store visited locations in db display visited state on map adding game settings to config adding municipality to data harvester using objects for locations instead of arrays adding a datarepository

---
+++ src/client/toServer.js
@@ -0,0 +1,25 @@
+/**
+ * we only have a socket connection when the user is locked in
+ * this wrapper checks this and ensures that there is no runtime error for non logged in users
+ * @param func
+ */
+const onlyWhenSocketAvailable = (func) => {
+    return function () {
+        if (!window.socket) {
+            return false;
+        }
+
+        func.apply(this, arguments);
+    };
+};
+
+
+/**
+ * claim that we visited a location
+ * @param locationId
+ */
+export const onVisitLocation = onlyWhenSocketAvailable((locationId) => {
+    window.socket.emit('visitedLocation', locationId);
+
+});
+
---
+++ src/dataRepository.js
@@ -0,0 +1,52 @@
+var memoize = require('memoizee');
+import logger from './helper/logger';
+
+
+/**
+ * the repository is responsible for all data sources and caches
+ * it aggregates those data for the application, a central point were data gets combined
+ * @param getModel
+ * @returns {{getAllLocationsForUser: (function())}}
+ */
+export default module.exports = (getModel) => {
+
+    const Location = getModel('location');
+    const User = getModel('user');
+
+    //caching is in the responsibility of the dataRepository
+    const cache = {
+        Location: {
+            getAllAsObject: memoize(Location.getAllAsObject, { maxAge: 15 * 60 * 1000 })
+        }
+    };
+
+
+    /**
+     * we need all locations with the individual sugar of a given user
+     * @param user
+     * @returns {Promise|Promise.<T>}
+     */
+    const getAllLocations = () => {
+        return cache.Location.getAllAsObject()
+            .catch((e)=> {
+                logger.error(e);
+            });
+
+    };
+
+    /**
+     * get a single user based on the ID
+     * @param userId
+     * @returns {Promise}
+     */
+    const findUserById = (userId) => {
+        return User.findOne(userId).exec();
+    };
+
+
+    return {
+        getAllLocations,
+        findUserById
+    };
+
+};
