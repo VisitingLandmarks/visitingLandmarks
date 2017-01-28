858f71eae5ba6b89f72793b726e6fe110c53b8d4
Pascal Pflaum <mail@pascalpflaum.de>
2017-01-28 23:22:19 +0100

    setup IO

--- src/app.js
+++ src/app.js
@@ -1,25 +1,18 @@
 import config from '../config';
 import setupRoutes from './routes';
-import setIO from './modules/sendActionToAllConnectionOfAUser';
+import {setupIO} from './modules/sendActionToAllConnectionOfAUser';
 
 //setup express
 const {app, server} = require('./modules/express')(config.port);
-
-//setup routes
-setupRoutes(app);
 
 //setup datarepository
 const data = require('./data');
 
 //setup io
-const io = require('socket.io')(server);
-setIO(io);
+setupIO(server);
 
-//sugar a wrapper to send an event to all connections of a given user
-const sendActionToAllConnectionOfAUser = require('./modules/sendActionToAllConnectionOfAUser');
-
-//handle socket.io requests of the user
-io.on('connection', require('./handleSocketIORequests')(sendActionToAllConnectionOfAUser, data));
+//setup routes
+setupRoutes(app);
 
 //setup authentication
 require('./modules/authentication')(app, io, data.User.serializeUser, data.User.deserializeUser, data.User.authenticate);
--- src/modules/sendActionToAllConnectionOfAUser.js
+++ src/modules/sendActionToAllConnectionOfAUser.js
@@ -1,22 +1,28 @@
 import passportSocketIo from 'passport.socketio';
 let getConnectionByUserId;
+import data from '../data';
 
 /**
  * a helper that will send a redux action to all connections of a user
  * @param userId
  * @param payload
  */
-export default module.exports = (userId, payload) => {
+export default sendActionToAllConnectionOfAUser;
+const sendActionToAllConnectionOfAUser = (userId, payload) => {
     getConnectionByUserId(userId).forEach(socket => {
         socket.emit('storeAction', payload);
     });
 };
 
-export const setIO = (io) => {
+export const setupIO = (server) => {
+    const ioApp = require('socket.io')(server);
     getConnectionByUserId = (userId) => {
         userId = userId && userId.toString();
-        return passportSocketIo.filterSocketsByUser(io, (user)=> {
+        return passportSocketIo.filterSocketsByUser(ioApp, (user)=> {
             return userId && user._id && userId === user._id.toString();
         });
     };
+    
+    //handle socket.io requests of the user
+    ioApp.on('connection', require('../handleSocketIORequests')(sendActionToAllConnectionOfAUser, data));
 };
