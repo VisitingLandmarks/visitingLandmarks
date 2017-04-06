0b212e84c28b6cfeb623f430df02e9e206691ae8
Pascal Pflaum <mail@pascalpflaum.de>
2017-04-06 22:00:51 +0200

    adding google login

--- config/envToConfig.js
+++ config/envToConfig.js
@@ -12,5 +12,9 @@ module.exports = {
             clientID: process.env.FACEBOOK_CLIENT_ID,
             clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
         },
+        google : {
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        },
     },
 };
